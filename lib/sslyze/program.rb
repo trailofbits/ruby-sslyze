47a93d5e529f7fcf2e523e8f7aef68bc898beaf9
Postmodern <postmodern.mod3@gmail.com>
2014-12-15 13:48:19 -0800

    Added a class level analysis method.

--- lib/sslyze/program.rb
+++ lib/sslyze/program.rb
@@ -8,6 +8,32 @@ module SSLyze
     name_program 'sslyze.py'
 
     #
+    # Finds the `sslyze.py` script and runs it.
+    #
+    # @param [Hash{Symbol => Object}] options
+    #   Additional options for `sslyze.py`.
+    #
+    # @param [Hash{Symbol => Object}] exec_options
+    #   Additional exec-options.
+    #
+    # @yield [task]
+    #   If a block is given, it will be passed a task object
+    #   used to specify options for `sslyze.py`.
+    #
+    # @yieldparam [Task] task
+    #   The sslyze task object.
+    #
+    # @return [Boolean]
+    #   Specifies whether the command exited normally.
+    #
+    # @see http://rubydoc.info/gems/rprogram/0.3.0/RProgram/Program#run-instance_method
+    #   For additional exec-options.
+    #
+    def self.analysis(options={},exec_options={},&block)
+      find.analysis(options,exec_options,&block)
+    end
+
+    #
     # Runs `sslyze.py`.
     #
     # @param [Hash{Symbol => Object}] options
