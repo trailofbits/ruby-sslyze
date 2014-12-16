e3cf278b271f84a6c331ac873e443c57af408b46
Postmodern <postmodern.mod3@gmail.com>
2014-12-15 23:34:31 -0800

    More inner_text fixes.

--- lib/sslyze/certificate.rb
+++ lib/sslyze/certificate.rb
@@ -14,17 +14,17 @@ module SSLyze
 
       def public_key
         @public_key ||= PublicKey.new(
-          @node.at('publicKey/modulus').value,
-          @node.at('publicKey/exponent').value.to_i
+          @node.at('publicKey/modulus').inner_text,
+          @node.at('publicKey/exponent').inner_text.to_i
         )
       end
 
       def public_key_algorithm
-        @public_key_algorithm ||= @node.at('publicKeyAlgorithm').value
+        @public_key_algorithm ||= @node.at('publicKeyAlgorithm').inner_text
       end
 
       def public_key_size
-        @public_key_size ||= @node.at('publicKeySize').value.chomp(' bits').to_i
+        @public_key_size ||= @node.at('publicKeySize').inner_text.chomp(' bits').to_i
       end
 
     end
@@ -36,46 +36,46 @@ module SSLyze
       end
 
       def organizational_unit_name
-        @organizational_unit_name ||= @node.at('organizationalUnitName').value
+        @organizational_unit_name ||= @node.at('organizationalUnitName').inner_text
       end
 
       def organization_name
-        @organization_name ||= @node.at('organizationalName').value
+        @organization_name ||= @node.at('organizationalName').inner_text
       end
 
       def business_category
-        @business_category ||= @node.at('businessCategory').value
+        @business_category ||= @node.at('businessCategory').inner_text
       end
 
       def serial_number
-        @serial_number ||= @node.at('serialNumber').value.to_i
+        @serial_number ||= @node.at('serialNumber').inner_text.to_i
       end
 
       def common_name
-        @common_name ||= @node.at('commonName').value
+        @common_name ||= @node.at('commonName').inner_text
       end
 
       def state_or_province_name
-        @state_or_province_name ||= @node.at('stateOrProvinceName').value
+        @state_or_province_name ||= @node.at('stateOrProvinceName').inner_text
       end
 
       def country_name
-        @country_name ||= @node.at('countryName').value
+        @country_name ||= @node.at('countryName').inner_text
       end
 
       def street_address
-        @street_address ||= @node.at('streetAddress').value
+        @street_address ||= @node.at('streetAddress').inner_text
       end
 
       # TODO: oid-1.3.6.1.4.1.311.60.2.1.2
       # TODO: oid-1.3.6.1.4.1.311.60.2.1.3
 
       def locality_name
-        @locality_name ||= @node.at('localityName').value
+        @locality_name ||= @node.at('localityName').inner_text
       end
 
       def postal_code
-        @postal_code ||= @node.at('postalCode').value
+        @postal_code ||= @node.at('postalCode').inner_text
       end
 
     end
@@ -90,19 +90,19 @@ module SSLyze
       end
 
       def country_name
-        @country_name || @node.at('countryName').value
+        @country_name || @node.at('countryName').inner_text
       end
 
       def common_name
-        @common_name ||= @node.at('commonName').value
+        @common_name ||= @node.at('commonName').inner_text
       end
 
       def organizational_unit_name
-        @organizational_unit_name ||= @node.at('organizationalUnitName').value
+        @organizational_unit_name ||= @node.at('organizationalUnitName').inner_text
       end
 
       def organization_name
-        @organization_name ||= @node.at('organizationName').value
+        @organization_name ||= @node.at('organizationName').inner_text
       end
 
     end
@@ -138,15 +138,15 @@ module SSLyze
     end
 
     def signature_value
-      @signature_value ||= @node.at('signatureValue').value
+      @signature_value ||= @node.at('signatureValue').inner_text
     end
 
     def signature_algorithm
-      @signature_algorithm ||= @node.at('signatureAlgorithm').value
+      @signature_algorithm ||= @node.at('signatureAlgorithm').inner_text
     end
 
     def serial_number
-      @serial_number ||= @node.at('serialNumber').value
+      @serial_number ||= @node.at('serialNumber').inner_text
     end
 
     def subject
@@ -155,8 +155,8 @@ module SSLyze
 
     def validity
       @validity ||= Validity.new(
-        Date.parse(@node.at('validity/notAfter').value),
-        Date.parse(@node.at('validity/notBefore').value)
+        Date.parse(@node.at('validity/notAfter').inner_text),
+        Date.parse(@node.at('validity/notBefore').inner_text)
       )
     end
 
