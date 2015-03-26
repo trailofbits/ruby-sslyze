b60414b6a5d4da8d631ff561238cab67dd0d5aae
Postmodern <postmodern.mod3@gmail.com>
2015-03-25 19:02:51 -0700

    Added more methods to OCSPResponse.

--- lib/sslyze/ocsp_response.rb
+++ lib/sslyze/ocsp_response.rb
@@ -1,5 +1,7 @@
 require 'sslyze/types'
 
+require 'time'
+
 module SSLyze
   #
   # Represents the `<ocspStapling>` XML element.
@@ -29,6 +31,51 @@ module SSLyze
 
     def path
       @path ||= @node.at('ocspResponse')
+    end
+
+    #
+    # The response type.
+    #
+    # @return [String]
+    #
+    def type
+      @type ||= @node.at('responseType').inner_text
+    end
+
+    #
+    # The response ID.
+    #
+    # @return [String]
+    #
+    def id
+      @id ||= @node.at('responseID').inner_text
+    end
+
+    #
+    # The OCSP version.
+    #
+    # @return [Integer]
+    #
+    def version
+      @version ||= @node.at('version').inner_text.to_i
+    end
+
+    #
+    # The response status.
+    #
+    # @return [Symbol]
+    #
+    def status
+      @status ||= @node.at('responseStatus').inner_text.to_sym
+    end
+
+    #
+    # When the response was produced.
+    #
+    # @return [Time]
+    #
+    def produced_at
+      @produced_at ||= Time.parse(@node.at('producedAt').inner_text)
     end
 
   end
