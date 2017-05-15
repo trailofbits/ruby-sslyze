19ae1d12ea878d4b5bca178c251c9556f60b8189
Postmodern <postmodern.mod3@gmail.com>
2017-05-15 15:56:07 -0700

    Updated the Certificate class.

--- lib/sslyze/xml/certificate.rb
+++ lib/sslyze/xml/certificate.rb
@@ -26,10 +26,12 @@ module SSLyze
       #
       # The position of the certificate within the cert chain.
       #
-      # @return [:leaf, :intermediate]
+      # @return [Symbol, nil]
       #
       def position
-        @position ||= @node['position'].to_sym
+        @position ||= if (value = @node['position'])
+                        value.to_sym
+                      end
       end
 
       #
@@ -42,98 +44,23 @@ module SSLyze
       end
 
       #
+      # The HPKP SHA256 Pin.
+      #
+      # @return [String]
+      #
+      # @since 1.0.0
+      #
+      def hpkp_sha256_pin
+        @hpkp_sha256_pin ||= @node['hpkpSha256Pin']
+      end
+
+      #
       # The AS PEM information.
       #
       # @return [String]
       #
       def as_pem
         @as_pem ||= @node.at('asPEM').inner_text
-      end
-
-      #
-      # The subject public key information.
-      #
-      # @return [SubjectPublicKeyInfo]
-      #
-      def subject_public_key_info
-        @subject_public_key_info ||= SubjectPublicKeyInfo.new(
-          @node.at('subjectPublicKeyInfo')
-        )
-      end
-
-      #
-      # The certificate SSL version.
-      #
-      # @return [Integer]
-      #
-      def version
-        @version ||= @node.at('version').inner_text.to_i
-      end
-
-      #
-      # The SSL extensions.
-      #
-      # @return [Extensions]
-      #
-      def extensions
-        @extensions ||= Extensions.new(@node.at('extensions'))
-      end
-
-      #
-      # The certificate signature.
-      #
-      # @return [String]
-      #
-      def signature_value
-        @signature_value ||= @node.at('signatureValue').inner_text
-      end
-
-      #
-      # The certificate signature algorithm.
-      #
-      # @return [String]
-      #
-      def signature_algorithm
-        @signature_algorithm ||= @node.at('signatureAlgorithm').inner_text
-      end
-
-      #
-      # The certificate serial number.
-      #
-      # @return [String]
-      #
-      def serial_number
-        @serial_number ||= @node.at('serialNumber').inner_text
-      end
-
-      #
-      # The certificate subject information.
-      #
-      # @return [Subject]
-      #
-      def subject
-        @subject ||= Subject.new(@node.at('subject'))
-      end
-
-      #
-      # The certificate validity.
-      #
-      # @return [Validity]
-      #
-      def validity
-        @validity ||= Validity.new(
-          Date.parse(@node.at('validity/notAfter').inner_text),
-          Date.parse(@node.at('validity/notBefore').inner_text)
-        )
-      end
-
-      #
-      # The certificate issuer.
-      #
-      # @return [Issuer]
-      #
-      def issuer
-        @issuer ||= Issuer.new(@node.at('issuer'))
       end
 
     end
