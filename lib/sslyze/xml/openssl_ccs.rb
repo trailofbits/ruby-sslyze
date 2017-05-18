7d6906381f69f8639e9739d29be9db464efc0899
Postmodern <postmodern.mod3@gmail.com>
2017-05-18 15:30:27 -0700

    Added XML::OpenSSLCCS for the `<openssl_ccs />` XML element.

---
+++ lib/sslyze/xml/openssl_ccs.rb
@@ -0,0 +1,33 @@
+require 'sslyze/xml/plugin'
+require 'sslyze/xml/is_vulnerable'
+
+module SSLyze
+  class XML
+    #
+    # Represents the `<openssl_ccs />` XML element.
+    #
+    # @since 1.0.0
+    #
+    class OpenSSLCCS < Plugin
+
+      #
+      # @return [OpenSSLCCSInjection]
+      #
+      def openssl_ccs_injection
+        @openssl_ccs_injection ||= OpenSSLCCSInjection.new(@node.at('openSslCcsInjection'))
+      end
+
+      alias injection openssl_ccs_injection
+
+      #
+      # @return [Boolean]
+      #
+      def is_vulnerable?
+        openssl_ccs_injection.is_vulnerable?
+      end
+
+      alias vulnerable? is_vulnerable?
+
+    end
+  end
+end
---
+++ lib/sslyze/xml/openssl_ccs/openssl_ccs_injection.rb
@@ -0,0 +1,23 @@
+require 'sslyze/xml/plugin'
+require 'sslyze/xml/attributes/is_vulnerable'
+
+module SSLyze
+  class XML
+    class OpenSSLCCS < Plugin
+      #
+      # Represents the `<openSslCcsInjection />` XML element.
+      #
+      # @since 1.0.0
+      #
+      class OpenSSLCCSInjection
+
+        include Attributes::IsVulnerable
+
+        def initialize(node)
+          @node = node
+        end
+
+      end
+    end
+  end
+end
