ef89eaead37aa2b826adf85e91acf62df0ca78b1
Postmodern <postmodern.mod3@gmail.com>
2017-05-18 15:30:04 -0700

    Added XML::Fallback for the `<fallback />` XML element.

---
+++ lib/sslyze/xml/fallback.rb
@@ -0,0 +1,25 @@
+require 'sslyze/xml/plugin'
+
+module SSLyze
+  class XML
+    class Fallback < Plugin
+
+      #
+      # @return [TLSFallbackSCSV]
+      #
+      def tls_fallback_scsv
+        @tls_fallback_scsv ||= TLSFallbackSCSV.new(@node.at('tlsFallbackScsv'))
+      end
+
+      #
+      # @see TLSFallbackSCSV#is_supported?
+      #
+      def is_supported?
+        tls_fallback_scsv.is_supported?
+      end
+
+      alias supported? is_supported?
+
+    end
+  end
+end
