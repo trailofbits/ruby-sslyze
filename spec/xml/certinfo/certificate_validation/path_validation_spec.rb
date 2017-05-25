d0ed417d813f23b8a32e59f921eaa0d74b4a4d02
Postmodern <postmodern.mod3@gmail.com>
2017-05-24 19:36:55 -0700

    Added missing specs for PathValidation.

--- spec/xml/certinfo/certificate_validation/path_validation_spec.rb
+++ spec/xml/certinfo/certificate_validation/path_validation_spec.rb
@@ -5,14 +5,16 @@ require 'sslyze/xml/certinfo/certificate_validation/path_validation'
 describe SSLyze::XML::Certinfo::CertificateValidation::PathValidation do
   include_examples "XML specs"
 
+  let(:xpath) { '/document/results/target/certinfo/certificateValidation/pathValidation' }
+
   subject do
-    described_class.new(xml.at('/document/results/target/certinfo/certificateValidation/pathValidation'))
+    described_class.new(xml.at(xpath))
   end
 
   describe "#verified_certificate_chain" do
     context "when 'verifiedCertificateChain' XML children are present" do
       subject do
-        described_class.new(xml.at('/document/results/target/certinfo/certificateValidation/pathValidation[verifiedCertificateChain]'))
+        described_class.new(xml.at("#{xpath}[verifiedCertificateChain]"))
       end
 
       it do
@@ -22,7 +24,7 @@ describe SSLyze::XML::Certinfo::CertificateValidation::PathValidation do
 
     context "when there are no 'verifiedCertificateChain' XML children" do
       subject do
-        described_class.new(xml.at('/document/results/target/certinfo/certificateValidation/pathValidation[not(verifiedCertificateChain)]'))
+        described_class.new(xml.at("#{xpath}[not(verifiedCertificateChain)]"))
       end
 
       it { expect(subject.verified_certificate_chain).to be nil }
@@ -32,7 +34,7 @@ describe SSLyze::XML::Certinfo::CertificateValidation::PathValidation do
   describe "#is_extended_validation_certificate?" do
     context "when the 'isExtendedValidationCertificate' XML attribute is present" do
       subject do
-        described_class.new(xml.at('/document/results/target/certinfo/certificateValidation/pathValidation[@isExtendedValidationCertificate]'))
+        described_class.new(xml.at("#{xpath}[@isExtendedValidationCertificate]"))
       end
 
       it "should return a Boolean type" do
@@ -42,10 +44,82 @@ describe SSLyze::XML::Certinfo::CertificateValidation::PathValidation do
 
     context "when the 'isExtendedValidationCertificate' XML attribute is omitted" do
       subject do
-        described_class.new(xml.at('/document/results/target/certinfo/certificateValidation/pathValidation[not(@isExtendedValidationCertificate)]'))
+        described_class.new(xml.at("#{xpath}[not(@isExtendedValidationCertificate)]"))
       end
 
       it { expect(subject.is_extended_validation_certificate?).to be nil }
+    end
+  end
+
+  describe "#trust_store_version" do
+    it "must return the 'trustStoreVersion' XML attribute" do
+      expect(subject.trust_store_version).to be == '7.0.0 r1'
+    end
+  end
+
+  describe "#using_trust_store" do
+    it "must return the 'usingTrustStore' XML attribute" do
+      expect(subject.using_trust_store).to be == 'AOSP'
+    end
+  end
+
+  describe "#validation_result" do
+    context "when the 'validationResult' attribute is set" do
+      subject do
+        described_class.new(xml.at("#{xpath}[@validationResult]"))
+      end
+
+      it "must return the 'validationResult' XML element" do
+        expect(subject.validation_result).to be == :ok
+      end
+    end
+
+    context "when the 'validationResult' attribute is not set" do
+      subject do
+        described_class.new(xml.at("#{xpath}[not(@validationResult)]"))
+      end
+
+      it "must return the 'validationResult' XML element" do
+        pending "need an example without the 'validationResult' XML attribute"
+
+        expect(subject.validation_result).to be nil
+      end
+    end
+  end
+
+  describe "#ok?" do
+    context "when the 'validationResult' attribute is set" do
+      context "and is 'ok'" do
+        subject do
+          described_class.new(xml.at("#{xpath}[@validationResult=\"ok\"]"))
+        end
+
+        it { expect(subject.ok?).to be true }
+      end
+
+      context "but is not 'ok'" do
+        subject do
+          described_class.new(xml.at("#{xpath}[not(@validationResult=\"ok\")]"))
+        end
+
+        it do
+          pending "need an example where the 'validationResult' isn't 'ok'"
+
+          expect(subject.ok?).to be false
+        end
+      end
+    end
+
+    context "when the 'validationResult' attribute is not set" do
+      subject do
+        described_class.new(xml.at("#{xpath}[not(@validationResult)]"))
+      end
+
+      it "must return the 'validationResult' XML element" do
+        pending "need an example without the 'validationResult' XML attribute"
+
+        expect(subject.ok?).to be nil
+      end
     end
   end
 end
