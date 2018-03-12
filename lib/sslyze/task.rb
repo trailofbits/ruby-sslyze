9b41a7e84b62b9a7c9e4bcec502cc17546c2c4fe
Postmodern <postmodern.mod3@gmail.com>
2018-03-12 12:16:19 -0700

    Rearrange options to match the order of `sslyze --help`.

--- lib/sslyze/task.rb
+++ lib/sslyze/task.rb
@@ -30,31 +30,19 @@ module SSLyze
     long_option flag: '--xmpp_to', equals: true
     long_option flag: '--sni', equals: true
 
+    # SessionResumptionPlugin:
+    long_option flag: '--resum'
+    long_option flag: '--resum_rate'
+
     # HeartbleedPlugin:
     long_option flag: '--heartbleed'
-
-    # OpenSslCcsInjectionPlugin:
-    long_option flag: '--openssl_ccs'
-
-    # FallbackScsvPlugin:
-    long_option flag: '--fallback'
-
-    # SessionRenegotiationPlugin:
-    long_option flag: '--reneg'
 
     # CertificateInfoPlugin:
     long_option flag: '--certinfo'
     long_option flag: '--ca_file', equals: true
 
-    # HttpHeadersPlugin:
-    long_option flag: '--http_headers'
-
-    # SessionResumptionPlugin:
-    long_option flag: '--resum'
-    long_option flag: '--resum_rate'
-
-    # CompressionPlugin:
-    long_option flag: '--compression'
+    # SessionRenegotiationPlugin:
+    long_option flag: '--reneg'
 
     # OpenSslCipherSuitesPlugin:
     long_option flag: '--sslv2'
@@ -64,6 +52,18 @@ module SSLyze
     long_option flag: '--tlsv1_2'
     long_option flag: '--http_get'
     long_option flag: '--hide_rejected_ciphers'
+
+    # HttpHeadersPlugin:
+    long_option flag: '--http_headers'
+
+    # CompressionPlugin:
+    long_option flag: '--compression'
+
+    # FallbackScsvPlugin:
+    long_option flag: '--fallback'
+
+    # OpenSslCcsInjectionPlugin:
+    long_option flag: '--openssl_ccs'
 
     non_option name: :targets, tailing: true
 
